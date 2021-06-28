42aead62e6a23dd6ac9b8f375251f8097a711472
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-06-28 21:29:37 +0530

    save edit data to local storage

--- scripts/index.js
+++ scripts/index.js
@@ -32,6 +32,7 @@ const newcard = ({
             <div class="card-footer text-muted">
                 <button
                 type="button"
+                id=${id}
                 class="btn btn-outline-primary float-end">
                     Open Task
                 </button>
@@ -132,7 +133,7 @@ const editCard = (event) => {
     const tagname = event.target.tagname;
 
     let parentElement;
-    if(tagname === "BUTTON"){
+    if (tagname === "BUTTON") {
         parentElement = event.target.parentNode.parentNode;
     }
     else {
@@ -146,16 +147,53 @@ const editCard = (event) => {
     let submitButton = parentElement.childNodes[7].childNodes[1];
 
     // setAttribute to change the attribute
-    taskTitle.setAttribute("contenteditable","true");
-    taskDescription.setAttribute("contenteditable","true");
-    taskType.setAttribute("contenteditable","true");
+    taskTitle.setAttribute("contenteditable", "true");
+    taskDescription.setAttribute("contenteditable", "true");
+    taskType.setAttribute("contenteditable", "true");
+    submitButton.setAttribute("onClick", "saveEditChanges.apply(this,arguments)");
     submitButton.innerHTML = "Save Changes";
-
 }
 
 
+const saveEditChanges = (event) => {
+    event = window.event;
+    const targetID = event.target.id;
+    const tagname = event.target.tagname;
 
+    let parentElement;
+    if (tagname === "BUTTON") {
+        parentElement = event.target.parentNode.parentNode;
+    }
+    else {
+        parentElement = event.target.parentNode.parentNode.parentNode;
+    }
 
+    let taskTitle = parentElement.childNodes[5].childNodes[1];
+    let taskDescription = parentElement.childNodes[5].childNodes[3];
+    let taskType = parentElement.childNodes[5].childNodes[5];
+    let submitButton = parentElement.childNodes[7].childNodes[1];
+
+    const updatedData = {
+        taskTitle: taskTitle.innerHTML,
+        taskType: taskType.innerHTML,
+        taskDescription: taskDescription.innerHTML,
+    }
+
+    globalStore = globalStore.map((task) => {
+        if (task.id === targetID) {
+            return {
+                id: task.id,
+                imageUrl: task.imageUrl,
+                taskTitle: updatedData.taskTitle,
+                taskType: updatedData.taskType,
+                taskDescription: updatedData.taskDescription
+            };
+        }
+        // if we don't return then the data in the globalStore may get deleted
+        return task; // important
+    });
+    updateLocalStorage();
+};
 
 // The modal was not closing upon adding new card
 // the cards were deleted afetr refresh -> localstorage 5mb
